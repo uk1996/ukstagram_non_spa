1f53cdb9fa10ab18d605440a8d61c79b4cb09f94
uk1996 <csuk1947@gmail.com>
2022-04-08 17:57:27 +0900

    프로젝트 생성 및 환경설정

---
+++ ukstagram_non_spa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ukstagram_non_spa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ukstagram_non_spa.settings.prod')
+
+application = get_asgi_application()
