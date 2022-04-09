761489ec0cebdedad83d480f1a1ae73d3979a4b1
SW Choi <csuk1947@gmail.com>
2022-04-09 21:55:20 +0900

    Bootstrap으로 layout 만들기

--- ukstagram_non_spa/urls.py
+++ ukstagram_non_spa/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +12,6 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
+        path('', TemplateView.as_view(template_name='root.html'), name='root')
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
