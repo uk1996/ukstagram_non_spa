55e5c85cbb55af777ebe63d95d4aaed9af6fa8f5
uk1996 <csuk1947@gmail.com>
2022-04-12 17:59:09 +0900

    로그인, 로그아웃, 회원가입하자마자 로그인

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
 from .forms import SignupForm
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST, request.FILES)
         if form.is_valid():
-            user = form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, 'ukstagram에 오신걸 환영합니다!')
             return redirect('root')
     else:
@@ -14,3 +17,9 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {
         'form':form,
     })
+
+login = LoginView.as_view(template_name='accounts/login_form.html')
+
+def logout(request):
+    messages.success(request, '로그아웃 되었습니다.')
+    return logout_then_login(request)
--- ukstagram_non_spa/settings/common.py
+++ ukstagram_non_spa/settings/common.py
@@ -126,6 +126,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
